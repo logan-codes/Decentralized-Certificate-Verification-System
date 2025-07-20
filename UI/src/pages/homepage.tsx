5d4e30f37783e379fa4b7c84290b2744b50fe80c
logan-codes <loganandamuthu2005@gmail.com>
2025-07-20 22:41:42 +0530

    The initialization of the Verichain project

---
+++ UI/src/pages/homepage.tsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Homepage</h1>
+      <p>This is the main page of our application.</p>
+    </div>
+  );
+};
+export default Home;
